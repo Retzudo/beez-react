9e8f8999b5f397f67ffeff156ea9e08acf863ce1
Retzudo <christoph.willinger@gmail.com>
2018-03-28 16:49:37 +0200

    removed index.css

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './reducers/rootReducer';
